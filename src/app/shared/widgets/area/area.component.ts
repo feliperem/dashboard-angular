a2665bf8a26c9e2f80cb79ac76bd754822d23254
feliperem <felipe.eufrasio@hotmail.com>
2021-01-18 17:59:30 -0300

    Correção da build

--- src/app/shared/widgets/area/area.component.ts
+++ src/app/shared/widgets/area/area.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-area',
@@ -15,63 +16,35 @@ export class AreaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.chartOptions  = 
-    {
-    chart: {
+    this.chartOptions = {
+        chart: {
           type: 'area'
-      },
-      title: {
-          text: 'Cômodos com maior consumo'
-      },
-      xAxis: {
-          categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-          tickmarkPlacement: 'on',
-          title: {
-              enabled: false
-          }
-      },
-      yAxis: {
-          title: {
-              text: 'kWh'
-          },
-          labels: {
-              formatter: function () {
-                  return this.value / 1000;
-              }
-          }
-      },
-      tooltip: {
+        },
+        title: {
+          text: 'Random DATA'
+        },
+        subtitle: {
+          text: 'Demo'
+        },
+        tooltip: {
           split: true,
-          valueSuffix: ' kWh'
-      },
-      plotOptions: {
-          area: {
-              stacking: 'normal',
-              lineColor: '#666666',
-              lineWidth: 1,
-              marker: {
-                  lineWidth: 1,
-                  lineColor: '#666666'
-              }
-          }
-      },
-      series: [{
-          name: 'Cozinha',
-          data: [502, 635, 809, 947, 1402, 3634, 5268]
-      }, {
-          name: 'Sala',
-          data: [106, 107, 111, 133, 221, 767, 1766]
-      }, {
-          name: 'Banheiro',
-          data: [163, 203, 276, 408, 547, 729, 628]
-      }, {
-          name: 'Quarto 1',
-          data: [18, 31, 54, 156, 339, 818, 1201]
-      }, {
-          name: 'Quarto 2',
-          data: [2, 2, 2, 6, 13, 30, 46]
-      }]
-    };
-  }
-
+          valueSuffix: ' millions'
+        },
+        credits: {
+          enabled: false
+        },
+        exporting: {
+          enabled: true,
+        },
+   
+      };
+  
+      HC_exporting(Highcharts);
+  
+      setTimeout(() => {
+        window.dispatchEvent(
+          new Event('resize')
+        );
+      }, 300);
+}
 }
